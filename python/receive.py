7d644198a827630c9ae45c3b56dbf5378ba57cbb
notepi <panpengok@126.com>
2016-12-15 15:39:19 +0800

    python目录可用于调试

--- python/receive.py
+++ python/receive.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python  
+# -*- coding: UTF-8 -*-
 import pika  
-  
+
+cQueue = "A"									#queue name
+cRouting_key = cQueue					#Routing_key
+cMessage = "m"								#meaasge body
+cExchange = "demo"						#exchange name  
+
 connection = pika.BlockingConnection(pika.ConnectionParameters(  
         host='localhost'))  
 channel = connection.channel()  
 
-channel.exchange_declare(exchange="demo",
-												 auto_delete=False)  
+#channel.exchange_declare(exchange="demo",
+#												 auto_delete=False)  
 												 
-#channel.queue_declare(queue='hello',
-#											auto_delete=True)  
+channel.queue_declare(queue=cQueue,
+											auto_delete=True)  
 											
-#channel.queue_bind( queue="hello",
-#										exchange="exchange",
-#										routing_key="hello" )
-#  
-#print ' [*] Waiting for messages. To exit press CTRL+C'  
-#  
-#def callback(ch, method, properties, body):  
-#    print " [x] Received %r" % (body,)  
-#  
-#channel.basic_consume(callback,  
-#                      queue='hello',  
-#                      no_ack=True)  
-#  
-#channel.start_consuming()  
+channel.queue_bind( queue=cQueue,
+										exchange=cExchange,
+										routing_key=cQueue )
+  
+print ' [*] Waiting for messages. To exit press CTRL+C'  
+  
+def callback(ch, method, properties, body):  
+    print " [x] Received %r" % (body,)  
+  
+channel.basic_consume(	callback,  
+			queue=cQueue,  
+			no_ack=True)  
+  
+channel.start_consuming()  
--- python/send.py
+++ python/send.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python  
+# -*- coding: UTF-8 -*-
 import pika  
-  
+
+cQueue = "B"									#queue name
+cRouting_key = cQueue					#Routing_key
+cMessage = "m"								#meaasge body
+cExchange = "demo"						#exchange name
+
+#connect to rabbitmq
 connection = pika.BlockingConnection(pika.ConnectionParameters(  
         host='localhost'))  
-channel = connection.channel()  
-  
+        
+#open rabbit channel
+channel = connection.channel()   
+
+#declare queue
 #channel.queue_declare(queue='hello',
 #											auto_delete=True)  
-  
-channel.basic_publish(exchange='hello-exchange',  
-                      routing_key='hello',  
-                      body='Hello World!')  
+
+#publish message 
+channel.basic_publish(exchange=cExchange,  
+                      routing_key=cRouting_key,  
+                      body=cMessage)  
 print " [x] Sent 'Hello World!'"  
 connection.close()
